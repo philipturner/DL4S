70a99b17b813291fc96d125bdfbb61cbe3d72cf1
Philip Turner <71743241+philipturner@users.noreply.github.com>
2021-11-08 07:42:21 -0500

    Initial Commit

---
+++ Sources/DL4S/DL4S.swift
@@ -0,0 +1,6 @@
+public struct DL4S {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/DL4STests/DL4STests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DL4S
+
+final class DL4STests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DL4S().text, "Hello, World!")
+    }
+}
